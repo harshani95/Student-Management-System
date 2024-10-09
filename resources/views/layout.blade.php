d653f1ef987a046304cef92a0752941d023c80a9
harshani <ruwanthikapemarathna@gmail.com>
2024-10-09 13:37:30 +0530

    added sideBar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,22 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><h2>Student Management System</h2></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> 
-    </div>
-  </div>
-</nav>
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#"><h2>Student Management System</h2></a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button> 
+                    </div>
+                </nav>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="sidebar">
+                    <a class="active" href="#home">Home</a>
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+                    <a href="#about">About</a>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="content">
+                     @yield('content')
+                </div>
+            </div>
     <div>
 </head>
 <body>
